1622334d2e7eb1c38891284b38c5165032461aba
etii <ethrowerii408@mustangs.deltacollege.edu>
2026-08-07 17:20:27 -0700

    update zoning vid

--- src/components/zoning-video-dialog.tsx
+++ src/components/zoning-video-dialog.tsx
@@ -6,7 +6,7 @@ import { CirclePlay, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { stopEmbeddedYouTubeVideo } from "@/lib/youtube";
 
-const YOUTUBE_ZONING_VIDEO_ID = "7SpodhGgnCw";
+const YOUTUBE_ZONING_VIDEO_ID = "XLq6BR3MRTw";
 const YOUTUBE_ZONING_VIDEO_URL =
     `https://www.youtube.com/embed/${YOUTUBE_ZONING_VIDEO_ID}?autoplay=1&modestbranding=1&rel=0&controls=1&enablejsapi=1`;
 
